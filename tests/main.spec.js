4b33e9694fe8ecb126c7d93a01ea6c9b7e7f2ed1
Danilo Augusto <dasf_cm@terra.com.br>
2019-12-03 17:51:13 -0300

    pratical examples of Mocha Hooks

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,31 +1,38 @@
 /* eslint-disable no-undef */
 describe('Main', function () {
+  var arr
   // Hooks
   // Before roda apenas uma vez antes do bloco
   before(function () {
-    console.log('before')
+    // Exemplo de uso: Inicia uma conexão com o banco de dados
   })
 
   // Roda uma vez depois do block
   after(function () {
-    console.log('after')
+    // Exemplo de uso: Fecha a conexão com o banco de dados
   })
 
   // Roda todas as vezes, antes de CADA bloco.
   beforeEach(function () {
-    console.log('before each')
+    arr = [1, 2, 3]
   })
 
   // Roda todas a vezes, depois de CADA bloco
   afterEach(function () {
-    console.log('after each')
+
   })
 
-  it('test 1', function () {
-    console.log('test 1')
+  it('should have a size of 4 when push another value to the array', function () {
+    arr.push(4)
+    console.log(arr.length) // 4
   })
 
-  it('test 2', function () {
-    console.log('test 2')
+  it('should have a size of 2 when pop a value from the array', function () {
+    arr.pop()
+    console.log(arr.length) // 2
+  })
+
+  it('should remove the value 3 when use pop in the array', function () {
+    console.log(arr.pop() === 3) // true
   })
 })
