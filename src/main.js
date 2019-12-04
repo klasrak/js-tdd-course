3fd7889b832542c1a210968cd8d23dcdc8a54c37
Danilo Augusto <dasf_cm@terra.com.br>
2019-12-04 10:00:45 -0300

    add babel and refactor to ES6+

--- src/main.js
+++ src/main.js
@@ -1,14 +1,6 @@
-module.exports = {
-  sum: function (n1, n2) {
-    return n1 + n2
-  },
-  sub: function (n1, n2) {
-    return n1 - n2
-  },
-  div: function (n1, n2) {
-    return (n2 === 0) ? 'Não é possível dividir por zero!' : n1 / n2
-  },
-  mul: function (n1, n2) {
-    return n1 * n2
-  }
-}
+const sum = (n1, n2) => n1 + n2
+const sub = (n1, n2) => n1 - n2
+const div = (n1, n2) => (n2 === 0) ? 'Não é possível dividir por zero!' : n1 / n2
+const mul = (n1, n2) => n1 * n2
+
+export { sum, sub, mul, div }
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,65 +1,61 @@
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-undef */
-const expect = require('chai').expect
-const calc = require('../src/main')
+import { expect } from 'chai'
+import { sum, sub, div, mul } from '../src/main'
 
 describe('Calc', () => {
   // smoke tests
   describe('smoke tests', () => {
-    it('should exist the "calc" lib', () => {
-      expect(calc).to.exist
-    })
-
     it('should exist the method "sum"', () => {
-      expect(calc.sum).to.exist
-      expect(calc.sum).to.be.a('function')
+      expect(sum).to.exist
+      expect(sum).to.be.a('function')
     })
 
     it('should exist the method "sub"', () => {
-      expect(calc.sub).to.exist
-      expect(calc.sub).to.be.a('function')
+      expect(sub).to.exist
+      expect(sub).to.be.a('function')
     })
 
     it('should exist the method "div"', () => {
-      expect(calc.div).to.exist
-      expect(calc.div).to.be.a('function')
+      expect(div).to.exist
+      expect(div).to.be.a('function')
     })
 
     it('should exist the method "mul"', () => {
-      expect(calc.mul).to.exist
-      expect(calc.mul).to.be.a('function')
+      expect(mul).to.exist
+      expect(mul).to.be.a('function')
     })
   })
 
   describe('Sum', () => {
     it('should return 4 if sum(2,2)', () => {
-      expect(calc.sum(2, 2)).to.be.equal(4)
+      expect(sum(2, 2)).to.be.equal(4)
     })
   })
 
   describe('Sub', () => {
     it('should return 4 if sub(6,2)', () => {
-      expect(calc.sub(6, 2)).to.be.equal(4)
+      expect(sub(6, 2)).to.be.equal(4)
     })
 
     it('should return -4 if sub(6,10)', () => {
-      expect(calc.sub(6, 10)).to.be.equal(-4)
+      expect(sub(6, 10)).to.be.equal(-4)
     })
   })
 
   describe('Div', () => {
     it('should return 4 if div(8,2)', () => {
-      expect(calc.div(8, 2)).to.be.equal(4)
+      expect(div(8, 2)).to.be.equal(4)
     })
 
     it('should return "Não é possível dividir por zero!" when divide by 0', () => {
-      expect(calc.div(4, 0)).to.be.equal('Não é possível dividir por zero!')
+      expect(div(4, 0)).to.be.equal('Não é possível dividir por zero!')
     })
   })
 
   describe('Mul', () => {
     it('should return 8 if mul(4,2)', () => {
-      expect(calc.mul(4, 2)).to.be.equal(8)
+      expect(mul(4, 2)).to.be.equal(8)
     })
   })
 })
